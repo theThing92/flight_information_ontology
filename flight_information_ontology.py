b402a0b3b809ed57960f7ec93aea870dd34b0fd3
Troublemaker343 <noonebuthappy@gmail.com>
2019-11-06 21:58:21 +0100

    added hongkong

--- flight_information_ontology.py
+++ flight_information_ontology.py
@@ -4,7 +4,7 @@ from owlready2 import *
 if __name__ == "__main__":
 
     # create empty ontology with some iri (internationalized ressource identifier)
-    onto = get_ontology("file:///some/path/flight_information_ontology.owl")
+    onto = get_ontology("flight_information_ontology.owl")
 
     # define the flight information ontolgy
     with onto:
@@ -34,6 +34,9 @@ if __name__ == "__main__":
             namespace = onto
 
         class Bundesstaat(Startort, Zielort):
+            namespace = onto
+
+        class Sonderverwaltungszone(Startort, Zielort):
             namespace = onto
 
         # define realations between concepts/classes
@@ -95,6 +98,10 @@ if __name__ == "__main__":
             domain = [Bundesstaat, Bundesland]
             range = [Stadt]
 
+        class besteht_aus_sonderverwaltungszonen(ObjectProperty):
+            domain = [Land]
+            range = [Sonderverwaltungszone]
+
         class ist_stadt_von_bundesland(ObjectProperty):
             domain = [Stadt]
             range = [Bundesland]
@@ -109,14 +116,18 @@ if __name__ == "__main__":
 
         start = Stadt(name="Düsseldorf", hat_synonyme=["DUS", "D-Dorf", "Ddorf"])
         ziel = Stadt(name="New York", hat_synonyme=["NY", "NYC"])
+        ziel2 = Stadt(name="Hongkong", hat_synonyme=["HK", "Hong Kong"])
 
         # countries
         amerika = Land(name="Amerika", hat_synonyme=["USA", "Vereinigte Staaten von Amerika"])
         deutschland =  Land(name="Deutschland", hat_synonyme=["Bundesrepublik Deutschland"])
+        china = Land(name="China", hat_synonyme=["Volksrepublik China", "Chinesische Repubrik", "Land der aufgehenden Sonne"])
 
         # federal staates
         düsseldorf_bundesland = Bundesland(name="Nord-Rhein-Westphalen", hat_synonyme=["NRW"])
         new_york_bundesstaat = Bundesstaat(name="New York", hat_synonyme=["NY"])
+        hongkong_sovezone = Sonderverwaltungszone(name="Hongkong", hat_synonyme = ["HK", "Hong Kong"])
+
 
         # airports
         düsseldorf_flughafen = Flughafen(name="Flughafen Düsseldorf", hat_synonyme=["Düsseldorf Flughafen", "Düsseldorf Airport","DUS"])
@@ -125,10 +136,14 @@ if __name__ == "__main__":
         new_york_ewr = Flughafen(name="Newark Liberty International Airport", hat_synonyme = ["EWR", " KEWR",  "New York-Newark International Airport"])
         new_york_lga = Flughafen(name="LaGuarda Airport", hat_synonyme=["LGA"])
 
+        hk_chep = Flughafen(name="Chek Lap Kok Hong Kong International Airport", hat_synonyme=["HKG", "Chek Lap Kok"])
+        hk_shek = Flughafen(name="Shek Kong Airfield", hat_synonyme=["Shek Kong", "VHSK"])
+
 
         # define relations between individuals
         reise.hat_einen_startort = start
         reise.hat_einen_zielort = ziel
+        reise.hat_einen_zielort = ziel2
 
         start.ist_gültiger_startflughafen_für_flugreise = False
         start.ist_gültiger_zielflughafen_für_flugreise = False
@@ -145,11 +160,20 @@ if __name__ == "__main__":
         new_york_lga.ist_gültiger_startflughafen_für_flugreise = False
         new_york_lga.ist_gültiger_zielflughafen_für_flugreise = True
 
+        hk_chep.ist_gültiger_startflughafen_für_flugreise = False
+        hk_chep.ist_gültiger_zielflughafen_für_flugreise = True
+
+        hk_shek.ist_gültiger_startflughafen_für_flugreise = False
+        hk_shek.ist_gültiger_zielflughafen_für_flugreise = True
+
         amerika.besteht_aus_bundesstaaten.append(new_york_bundesstaat)
         deutschland.besteht_aus_bundesländern.append(düsseldorf_bundesland)
+        china.besteht_aus_sonderverwaltungszonen.append(hongkong_sovezone)
 
 
         düsseldorf_flughafen.ist_flughafen_von.append(start)
         new_york_lga.ist_flughafen_von.append(ziel)
         new_york_ewr.ist_flughafen_von.append(ziel)
         new_york_jfk.ist_flughafen_von.append(ziel)
+        hk_chep.ist_flughafen_von.append(ziel)
+        hk_shek.ist_flughafen_von.append(ziel)
